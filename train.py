74b8539b29d41f6cff311c444016577498bc8862
JimmyNguyen09-AI <ng.trungthanh04@gmail.com>
2026-01-21 14:33:13 +1100

    them test.py, thay doi train.py

--- train.py
+++ train.py
@@ -7,6 +7,5 @@ x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=
 model = LogisticRegression(max_iter=200)
 model.fit(x_train,y_train)
 y_pred = model.predict(x_test)
-print(y_pred)
 acc = accuracy_score(y_test,y_pred)
 print("Accuracy",acc)
